64c07bf82c612a4a5424761d5ae356f981fc8762
Mel <asimbolalalaina@gmail.com>
2023-03-14 13:55:26 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/itu/rakotoasimbola/tpcustomer/ejb/CustomerManager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.rakotoasimbola.tpcustomer.ejb;
+
+import jakarta.ejb.Stateless;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import mg.itu.rakotoasimbola.tpcustomer.entities.Customer;
+
+/**
+ *
+ * @author Mel
+ * Gère la persistence des Customers
+ */
+@Stateless
+public class CustomerManager {
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
